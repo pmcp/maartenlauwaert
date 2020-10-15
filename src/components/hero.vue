7557f5ae5f4b90e0b8f6f82fe26a314f01749df7
PMCP <maartenlauwaert@gmail.com>
2020-10-15 20:08:20 +0200

    removed unused css

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -3,14 +3,8 @@
   <div class="container mx-auto m-15">
     <h2 class="text-3xl font-bold text-gray-800 tracking-tight pb-7 capitalize">What I can do for you</h2>
     <p class="mt-4  text-base leading-7 text-gray-800 max-w-2xl">
-      You don't need a fully working app to test what works or doesn't. I turn ideas into something you can put in front of real people. This can be a simple clickable design, a working application or a small tweak to an existing product. I try to minimize effort and maximize learnings.
+      You  don't need a fully working app to test what works or doesn't. I turn ideas into something you can put in front of real people. This can be a simple clickable design, a working application or a small tweak to an existing product. I try to minimize effort and maximize learnings.
     </p>
   </div>
 </div>
 </template>
-
-<script>
-export default {
- 
-}
-</script>

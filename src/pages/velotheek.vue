d6b7cda7aff0770808f7dd5908a4a252d332a4a0
PMCP <maartenlauwaert@gmail.com>
2020-10-15 20:08:30 +0200

    more unused css

--- src/pages/velotheek.vue
+++ src/pages/velotheek.vue
@@ -356,17 +356,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-.slide-fade-enter-active {
-  transition: all 0.3s ease;
-}
-.slide-fade-leave-active {
-  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
-}
-.slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active below version 2.1.8 */ {
-  transform: translateX(10px);
-  opacity: 0;
-}
-</style>

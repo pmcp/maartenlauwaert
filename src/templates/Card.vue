785232471fa4a7f542ebe6c6f671b4b4977e4ba4
PMCP <maartenlauwaert@gmail.com>
2020-10-18 16:45:53 +0200

    emptied the whole cards template

--- src/templates/Card.vue
+++ src/templates/Card.vue
@@ -1,12 +1,7 @@
 <template>
 
   <Layout>
-    
-    <ul>
-      <li>{{ $page.link.url }}</li>
-      <li>{{ $page.link.type }}</li>
-      <li>{{ $page.link.cover }}</li>
-    </ul>
+
   </Layout>
 
 </template>

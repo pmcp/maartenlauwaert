342eee47887e08b4aa128ac19f8815781c3ea3d6
PMCP <maartenlauwaert@gmail.com>
2020-10-18 12:29:10 +0200

    added but not using image for article comp

---
+++ src/components/NOTINUSE-articleImage.vue
@@ -0,0 +1,23 @@
+<template>
+<g-image
+      slot="image"
+      class="w-auto"
+      src="~/images/velotheek/velotheek_excalidraw_step1.png"
+      alt="An scheme showing the timeline of the project. Highlighted is step 1: Research"
+    />  
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      default: '' // TODO: Add placeholder
+    },
+    alt: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>

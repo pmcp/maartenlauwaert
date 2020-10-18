7ce8698bd6726746cefee4f55ec88806257e3e0c
PMCP <maartenlauwaert@gmail.com>
2020-10-18 16:45:12 +0200

    was removing the wrong name

--- src/templates/Card.vue
+++ src/templates/Card.vue
@@ -1,7 +1,7 @@
 <template>
 
   <Layout>
-    <h1 class="mt-2 mb-8 text-6xl leading-10 font-extrabold tracking-tight text-gray-900 sm:text-5xl sm:leading-tight">{{ $page.link.name }}</h1>
+    
     <ul>
       <li>{{ $page.link.url }}</li>
       <li>{{ $page.link.type }}</li>
@@ -15,6 +15,7 @@
 query ($id: ID!) {
   card(id: $id) {
     id
+    name
     url
     cat
     type

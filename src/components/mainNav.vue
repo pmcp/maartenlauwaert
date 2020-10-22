0d1fb76b76133ff60ceaab42914e652d10d6bf22
PMCP <maartenlauwaert@gmail.com>
2020-10-22 20:29:41 +0200

    test  with g-image vs a

--- src/components/mainNav.vue
+++ src/components/mainNav.vue
@@ -2,7 +2,7 @@
   <nav class="container text-gray-800 pl-4 sm:px-0 mx-auto flex flex-row flex-no-wrap text-sm py-5 sm:py-10 overflow-x-auto">
     <g-link class="" to="/">Home</g-link>
     <g-link class="pl-4  md:pl-8 whitespace-no-wrap" to="/about/">About me</g-link>
-    <g-link class="pl-4  md:pl-8 whitespace-no-wrap" to="/articles/">Work & Writings</g-link>
+    <a class="pl-4  md:pl-8 whitespace-no-wrap" href="/articles/">Work & Writings</a>
     <g-link class="pl-4 pr-4 md:pl-8 whitespace-no-wrap"  to="/glossary/">Tools & Terms</g-link>
   </nav>
 </template>

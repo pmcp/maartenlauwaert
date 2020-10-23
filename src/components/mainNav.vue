00e57944f32e8cf7c0fc2ac0947cdbf934fbe237
PMCP <maartenlauwaert@gmail.com>
2020-10-23 08:43:03 +0200

    stripping so whitespace

--- src/components/mainNav.vue
+++ src/components/mainNav.vue
@@ -1,8 +1,8 @@
 <template>
   <nav class="container text-gray-800 pl-4 sm:px-0 mx-auto flex flex-row flex-no-wrap text-sm py-5 sm:py-10 overflow-x-auto">
-    <g-link to="/" class="" >Home</g-link>
-    <g-link to="/about/" class="pl-4  md:pl-8 whitespace-no-wrap" >About me</g-link>
-    <g-link to="/articles/" class="pl-4  md:pl-8 whitespace-no-wrap" >Work & Writings</g-link>
-    <g-link to="/glossary/" class="pl-4 pr-4 md:pl-8 whitespace-no-wrap"  >Tools & Terms</g-link>
+    <g-link to="/" class="">Home</g-link>
+    <g-link to="/about/" class="pl-4  md:pl-8 whitespace-no-wrap">About me</g-link>
+    <g-link to="/articles/" class="pl-4  md:pl-8 whitespace-no-wrap">Work & Writings</g-link>
+    <g-link to="/glossary/" class="pl-4 pr-4 md:pl-8 whitespace-no-wrap">Tools & Terms</g-link>
   </nav>
 </template>

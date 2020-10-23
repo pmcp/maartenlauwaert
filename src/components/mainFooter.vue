4bc81557d6c1d220395ef4c44bf0b55918479468
PMCP <maartenlauwaert@gmail.com>
2020-10-23 09:56:36 +0200

    changed to footer

--- src/components/mainFooter.vue
+++ src/components/mainFooter.vue
@@ -1,12 +1,12 @@
 <template>
-<div class="bg-black p-15 ">
+<footer class="bg-black p-15 ">
   <div class="container mx-auto m-15">
     <h2 class="text-3xl font-bold text-white tracking-tight pb-7 capitalize">Maarten Lauwaert</h2>
     <p class="mt-4  text-base leading-7 text-white max-w-2xl">
       This website doesn’t use cookies, but  I do keep an eye on some analytics.
     </p>
   </div>
-</div>
+</footer>
 </template>
 
 <script>

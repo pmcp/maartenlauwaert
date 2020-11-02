27d1f5ca0cad4044d1df0cd6fa9e38b6633fe8da
PMCP <maartenlauwaert@gmail.com>
2020-11-02 20:56:54 +0100

    small responsive tweaks

--- src/components/mainNav.vue
+++ src/components/mainNav.vue
@@ -1,8 +1,28 @@
 <template>
-  <nav class="container bg-white text-gray-800 pl-6 sm:px-15 mx-auto flex flex-row flex-no-wrap text-sm pt-4 sm:py-10 overflow-x-auto">
-    <g-link to="/" class="">Home</g-link>
-    <g-link to="/about/" class="ml-2  md:ml-8 whitespace-no-wrap">About me</g-link>
-    <g-link to="/articles/" class="ml-2 md:ml-8 whitespace-no-wrap">Work & Writings</g-link>
-    <g-link to="/glossary/" class="ml-2 mr-2 md:ml-8 whitespace-no-wrap">Tools & Terms</g-link>
-  </nav>
+  <div>
+    <!-- <div class="sm:hiddem w-full flex flex-row   bg-black text-white p-4  text-sm ">
+      <span class="mx-auto container pl-2 sm:px-15 ">
+        Get in contact at hello@maartenlauwaert.eu
+      </span>
+
+    </div> -->
+    <nav class="container bg-white text-gray-800 pl-6 sm:px-15 mx-auto flex flex-row flex-no-wrap text-sm pt-4 sm:py-10 overflow-x-auto">
+      <g-link
+        to="/"
+        class=""
+      >Home</g-link>
+      <g-link
+        to="/about/"
+        class="ml-2  md:ml-8 whitespace-no-wrap"
+      >About me</g-link>
+      <g-link
+        to="/articles/"
+        class="ml-2 md:ml-8 whitespace-no-wrap"
+      >Work & Writings</g-link>
+      <g-link
+        to="/glossary/"
+        class="ml-2 mr-2 md:ml-8 whitespace-no-wrap"
+      >Tools & Terms</g-link>
+    </nav>
+  </div>
 </template>
--- src/pages/Articles.vue
+++ src/pages/Articles.vue
@@ -2,7 +2,7 @@
   <Layout>
     <h1 class="px-6 sm:px-15 container mx-auto mt-2 mb-6 sm:mb-14 font-extrabold tracking-tight text-gray-900 text-3xl sm:text-5xl leading-snug sm:leading-tight">Work & Writings</h1>
 
-    <div class="container mx-auto px-6 sm:px-15 ">
+    <div class="container mx-auto px-6 sm:px-15 mb-28">
       <h2 class="text-3xl font-bold text-gray-800 tracking-tight pb-7 capitalize">Case studies</h2>
       <div class="grid gap-16 pt-10 lg:grid-cols-2 lg:gap-x-5 lg:gap-y-12">
         <div
